fab711af6346fcf71e521fc179ac261d08f73f53
ThongVA <anhthongvu1996@gmail.com>
2025-03-08 01:24:22 +0700

    update config nvim ufo

--- nvim/.config/nvim/lua/config/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/config/plugins/nvim-ufo.lua
@@ -3,9 +3,6 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	event = "VeryLazy",
 	opts = {
-		provider_selector = function(bufnr, filetype, buftype)
-			return { "treesitter", "indent" }
-		end,
 		open_fold_hl_timeout = 400,
 		close_fold_kinds_for_ft = { "imports", "comment" },
 		preview = {
@@ -13,14 +10,6 @@ return {
 				border = "rounded",
 				winblend = 0,
 				winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
-
-				-- Positioning the preview window
-				relative = "editor",
-				anchor = "NE",
-				row = math.floor(vim.o.lines * 0.25), -- Start at 25% of the screen height
-				col = math.floor(vim.o.columns * 0.7), -- Move to the right (70% of screen width)
-				width = math.max(30, math.floor(vim.o.columns * 0.3)), -- At least 30 columns wide
-				height = math.max(10, math.floor(vim.o.lines * 0.5)), -- At least 10 lines tall
 				maxheight = math.floor(vim.o.lines * 0.5),
 			},
 			mappings = {
@@ -32,12 +21,6 @@ return {
 		},
 	},
 	init = function()
-		-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-		-- vim.o.foldcolumn = "1"
-		-- vim.o.foldlevel = 99
-		-- vim.o.foldlevelstart = 99
-		-- vim.o.foldenable = true
-
 		vim.o.foldcolumn = "1" -- Always show fold column
 		vim.o.foldlevel = 99
 		vim.o.foldlevelstart = 99
@@ -47,9 +30,7 @@ return {
 	config = function(_, opts)
 		local handler = function(virtText, lnum, endLnum, width, truncate)
 			local newVirtText = {}
-			local totalLines = vim.api.nvim_buf_line_count(0)
-			local foldedLines = endLnum - lnum
-			local suffix = (" 󰁂 %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
 			local sufWidth = vim.fn.strdisplaywidth(suffix)
 			local targetWidth = width - sufWidth
 			local curWidth = 0
@@ -70,8 +51,6 @@ return {
 				end
 				curWidth = curWidth + chunkWidth
 			end
-			local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
-			suffix = (" "):rep(rAlignAppndx) .. suffix
 			table.insert(newVirtText, { suffix, "MoreMsg" })
 			return newVirtText
 		end
