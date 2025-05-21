b080c6ec88776478cbc1ac69a27f860cd696ab0e
ThongVA <thongva@regit-technology.com>
2025-05-21 09:44:33 +0700

    find .env use snack picker file

--- nvim/.config/nvim/lua/config/plugins/snacks.lua
+++ nvim/.config/nvim/lua/config/plugins/snacks.lua
@@ -195,7 +195,10 @@ return {
     {
       "<leader>ff",
       function()
-        Snacks.picker.files()
+        Snacks.picker.files({
+          hidden = true,
+          ignored = true,
+        })
       end,
       desc = "Find Files",
     },
