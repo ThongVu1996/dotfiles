dae498721cc3a3266927e9a7c6cd22e4152ba301
Thống Vũ <thongvu@MacBook-Pro.local>
2025-11-26 15:34:52 +0700

    feat: update reload nvim, wezterm realtime, don't need rebuild

--- home.nix
+++ home.nix
@@ -20,12 +20,17 @@
   # Cú pháp: xdg.configFile."TÊN_TRONG_CONFIG".source = ĐƯỜNG_DẪN_THỰC_TẾ;
    xdg.configFile."starship.toml".source = ./dotfiles/starship/.config/starship.toml;
    xdg.configFile."lazygit/config.yml".source = ./dotfiles/lazygit/.config/lazygit/config.yml;
-   xdg.configFile."nvim".source = ./dotfiles/nvim/.config/nvim;
+   # xdg.configFile."nvim".source = ./dotfiles/nvim/.config/nvim;
+   # xdg.configFile."wezterm".source = ./dotfiles/wezterm/.config/wezterm;
    xdg.configFile."fish/config.fish".source = ./dotfiles/fish/.config/fish/config.fish;
-   xdg.configFile."wezterm".source = ./dotfiles/wezterm/.config/wezterm;
    xdg.configFile."aerospace/aerospace.toml".source = ./dotfiles/aerospace/.config/aerospace/aerospace.toml;
    home.file.".tmux.conf".source = ./dotfiles/tmux/.tmux.conf;
 
+   # 1. Neovim (Dùng mkOutOfStoreSymlink để sửa là ăn ngay)
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "/Users/${username}/nix-config/dotfiles/nvim/.config/nvim";
+
+  # 2. WezTerm (Tương tự)
+  xdg.configFile."wezterm".source = config.lib.file.mkOutOfStoreSymlink "/Users/${username}/nix-config/dotfiles/wezterm/.config/wezterm";
 
   # 3. Kích hoạt Home Manager
   programs.home-manager.enable = true;
