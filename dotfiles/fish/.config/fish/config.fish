f123387b92e7bbbd6d1aff0e24fd165244740c6f
thongvu <anhthongvu1996@gmail.com[D>
2025-12-05 23:54:20 +0700

    config eza to see folder structure

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -33,6 +33,15 @@ set -Ux STARSHIP_CONFIG ~/.config/starship.toml
 # open nvim
 alias vi='nvim'
 alias ls='eza'
+function ls --wraps eza
+    # Kiểm tra: Nếu chỉ có đúng 1 tham số VÀ tham số đó là Số (ví dụ: 1, 2, 3...)
+    if test (count $argv) -eq 1; and string match -qr '^[0-9]+$' -- "$argv[1]"
+        eza --tree --level=$argv[1]
+    else
+        # Các trường hợp còn lại (ls, ls -la, ls /tmp...) chạy eza bình thường
+        eza $argv
+    end
+end
 alias lz='lazygit'
 # System Rebuild Alias
 alias nixss="sudo nix run nix-darwin -- switch --flake ~/nix-config#(scutil --get LocalHostName)"
