7166d119e04d0a2d35d972a31b1ddfb3017a9cc9
thongvu <anhthongvu1996@gmail.com[D>
2025-12-06 10:36:35 +0700

    add snipset for js, reactjs
    change keymap choose options bink to use TAB for snipset

--- dotfiles/nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -1,21 +1,47 @@
 return {
-  "saghen/blink.cmp",
-  event = { "LspAttach" },
-  dependencies = "rafamadriz/friendly-snippets",
-  version = "v0.*",
-  opts = {
-    keymap = {
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-    },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
-    },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-    signature = { enabled = true },
-  },
+	"saghen/blink.cmp",
+	event = { "LspAttach" },
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "v0.*",
+	opts = {
+		keymap = {
+			-- ["<S-Tab>"] = { "select_prev", "fallback" },
+			-- ["<Tab>"] = { "select_next", "fallback" },
+
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "snippets", "lsp", "path", "buffer" },
+		},
+		signature = { enabled = true },
+		completion = {
+			documentation = {
+				-- 1. Tự động hiển thị màn hình phụ (True = Bật)
+				auto_show = true,
+
+				-- 2. Thời gian chờ (Delay) trước khi hiện (ms)
+				-- Để 0 thì hiện ngay lập tức, để 200 cho đỡ nháy mắt khi lướt nhanh
+				auto_show_delay_ms = 200,
+
+				-- 3. Cấu hình giao diện cửa sổ
+				window = {
+					border = "rounded", -- Viền bo tròn cho đẹp (hoặc "single", "double")
+				},
+			},
+
+			-- (Tùy chọn) Cấu hình menu danh sách cho gọn đẹp hơn
+			menu = {
+				border = "rounded",
+				draw = {
+					columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+				},
+			},
+		},
+	},
 }
