eab6f23134519cd317ca5bc6107dba9d528118cd
thongvu <anhthongvu1996@gmail.com[D>
2025-12-15 16:54:41 +0700

    change transparent colorscheme and change shortcut format code

--- dotfiles/nvim/.config/nvim/lua/config/lsp/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/config/lsp/keymaps.lua
@@ -5,9 +5,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename Symbol" }))
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<leader>f", function()
+		vim.keymap.set("n", "<leader>fm", function()
 			vim.lsp.buf.format({ async = true })
-		end, opts)
+		end, opts, { desc = "Format code manual" })
 	end,
 })
 
