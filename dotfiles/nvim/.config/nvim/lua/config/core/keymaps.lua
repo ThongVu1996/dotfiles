250e9eaaddc68e6964cde14f9a5977590aafde03
thongvu <anhthongvu1996@gmail.com[D>
2025-12-15 19:17:00 +0700

    center after move screen, smart screen

--- dotfiles/nvim/.config/nvim/lua/config/core/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/config/core/keymaps.lua
@@ -5,6 +5,9 @@ local keymap = vim.keymap -- Alias for conciseness
 -- Exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk", noremap = true, silent = true })
 
+-- Copy into end of the line
+vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
+
 -- Clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights", noremap = true, silent = true })
 
@@ -42,10 +45,62 @@ keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, de
 
 -- Center view
 
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
-keymap.set("n", "*", "*zzzv", { desc = "Search word under cursor + center" })
-keymap.set("n", "#", "#zzzv", { desc = "Search word backward + center" })
+local function smart_page_scroll(direction)
+	local win_height = vim.api.nvim_win_get_height(0)
+	local cur_line = vim.api.nvim_win_get_cursor(0)[1]
+	local total_lines = vim.api.nvim_buf_line_count(0)
+
+	if direction == "down" then
+		-- Nếu trang tiếp theo sẽ vượt quá cuối file
+		if cur_line + win_height >= total_lines then
+			-- Nhảy thẳng xuống dòng cuối cùng (G)
+			vim.cmd("normal! G")
+			-- Và ghim dòng đó ở ĐÁY màn hình (zb) để lấp đầy code phía trên
+			vim.cmd("normal! zb")
+		else
+			-- Nếu chưa hết file thì cuộn C-f bình thường
+			vim.cmd([[execute "normal! \<C-f>"]])
+		end
+	else
+		-- Cuộn lên (C-b) thì giữ nguyên mặc định
+		vim.cmd([[execute "normal! \<C-b>"]])
+	end
+end
+
+-- 1. TÌM KIẾM (SEARCH) - GIỮ NGUYÊN
+-- Dùng function để tránh lỗi linter báo "unknown word"
+vim.keymap.set("n", "n", function()
+	vim.cmd("normal! nzzzv")
+end, { desc = "Next result (centered)" })
+vim.keymap.set("n", "N", function()
+	vim.cmd("normal! Nzzzv")
+end, { desc = "Prev result (centered)" })
+vim.keymap.set("n", "*", function()
+	vim.cmd("normal! *zzzv")
+end, { desc = "Search word (centered)" })
+vim.keymap.set("n", "#", function()
+	vim.cmd("normal! #zzzv")
+end, { desc = "Search backward (centered)" })
+
+-- 2. CUỘN NỬA TRANG (HALF PAGE) - CÓ ZZ
+-- Giữ 'zz' để mắt luôn nhìn vào giữa màn hình
+vim.keymap.set("n", "<C-d>", function()
+	vim.cmd([[execute "normal! \<C-d>zz"]])
+end, { desc = "Half page down (centered)" })
+
+vim.keymap.set("n", "<C-u>", function()
+	vim.cmd([[execute "normal! \<C-u>zz"]])
+end, { desc = "Half page up (centered)" })
+
+-- 3. CUỘN CẢ TRANG (FULL PAGE) - SMART SCROLL (ĐÃ FIX)
+-- Sử dụng hàm thông minh để không bị lỗi hiển thị 1 dòng cuối file
+vim.keymap.set("n", "<C-f>", function()
+	smart_page_scroll("down")
+end, { desc = "Smart Page down" })
+
+vim.keymap.set("n", "<C-b>", function()
+	smart_page_scroll("up")
+end, { desc = "Smart Page up" })
 
 vim.api.nvim_create_autocmd("CmdlineLeave", {
 	group = vim.api.nvim_create_augroup("AutoCenterSearch", { clear = true }),
