da8948ebd177c46569c37c64e106615088dd2274
thongvu <anhthongvu1996@gmail.com[D>
2026-01-28 17:06:44 +0700

    add antigravity and obrstack(docker desktop)

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -465,3 +465,10 @@ end
             return 1
     end
 end
+
+# Added by Antigravity
+fish_add_path /Users/thongvu/.antigravity/antigravity/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
