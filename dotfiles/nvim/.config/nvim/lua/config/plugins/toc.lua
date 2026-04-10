af6705c416a5b27b40a68e612d164bf073151295
thongvu <anhthongvu1996@gmail.com[D>
2026-04-10 13:45:36 +0700

    chore: remove trash

--- dotfiles/nvim/.config/nvim/lua/config/plugins/toc.lua
+++
@@ -1,8 +0,0 @@
-return {
-	dir = vim.fn.stdpath("config") .. "/lua/modules/toc",
-	name = "vscode-strict-toc",
-	ft = "markdown",
-	config = function()
-		require("modules.toc.vscode_strict_toc").setup()
-	end,
-}
