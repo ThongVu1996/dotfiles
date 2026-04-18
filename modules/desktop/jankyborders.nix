e26071c742b2f0aebc4b8869a399057e3990dd2c
thongvu <anhthongvu1996@gmail.com[D>
2026-04-18 20:17:44 +0700

    feat: add jankyboders

--- modules/desktop/jankyborders.nix
+++ modules/desktop/jankyborders.nix
@@ -10,15 +10,23 @@ in {
     enable = lib.mkEnableOption "Enable JankyBorders";
     width = lib.mkOption {
       type = lib.types.float;
-      default = 6.0;
+      default = 3.0;
     };
     active_color = lib.mkOption {
       type = lib.types.str;
-      default = "0xffe1e1e1";
+      default = "gradient(top_right=0xff8aadf4,bottom_left=0xffc6a0f6)";
     };
     inactive_color = lib.mkOption {
       type = lib.types.str;
-      default = "0xff494d64";
+      default = "0xff313244";
+    };
+    style = lib.mkOption {
+      type = lib.types.str;
+      default = "round";
+    };
+    blur_radius = lib.mkOption {
+      type = lib.types.float;
+      default = 10.0;
     };
   };
 
