5cf3306e513ba95a274637ccd4cdf89a4a51225b
thongvu <anhthongvu1996@gmail.com[D>
2026-05-20 15:47:00 +0700

    replace  autopair and rainbow by blink.pairs

--- dotfiles/nvim/.config/nvim/lua/config/plugins/code/autopairs.lua
+++ dotfiles/nvim/.config/nvim/lua/config/plugins/code/autopairs.lua
@@ -1,17 +1,37 @@
+-- return {
+-- 	"windwp/nvim-autopairs",
+-- 	event = { "InsertEnter" },
+-- 	dependencies = {
+-- 		"saghen/blink.cmp",
+-- 	},
+-- 	config = function()
+-- 		require("nvim-autopairs").setup({
+-- 			check_ts = true,
+-- 			ts_config = {
+-- 				lua = { "string" },
+-- 				javascript = { "template_string" },
+-- 				java = false,
+-- 			},
+-- 		})
+-- 	end,
+-- }
+
+
 return {
-	"windwp/nvim-autopairs",
-	event = { "InsertEnter" },
+	"saghen/blink.pairs",
+	version = "*", -- Bắt buộc để tải bản build sẵn
+	event = "InsertEnter",
 	dependencies = {
-		"saghen/blink.cmp",
+		"saghen/blink.download",
 	},
-	config = function()
-		require("nvim-autopairs").setup({
-			check_ts = true,
-			ts_config = {
-				lua = { "string" },
-				javascript = { "template_string" },
-				java = false,
-			},
-		})
-	end,
+	opts = {
+		mappings = {
+			enabled = true,
+			cmdline = true,
+		},
+		highlights = {
+			enabled = true,
+			cmdline = true,
+		},
+	},
 }
