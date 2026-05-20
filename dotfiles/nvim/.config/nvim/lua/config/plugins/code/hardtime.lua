d13f494ef233cc66147611b82deac009b6ca9e7c
thongvu <anhthongvu1996@gmail.com[D>
2026-05-20 16:48:24 +0700

    ignore check for grub-far.nvim

--- dotfiles/nvim/.config/nvim/lua/config/plugins/code/hardtime.lua
+++ dotfiles/nvim/.config/nvim/lua/config/plugins/code/hardtime.lua
@@ -1,5 +1,20 @@
 return {
 	"m4xshen/hardtime.nvim",
 	event = "BufReadPost",
-	opts = {},
+	opts = {
+		disabled_filetypes = {
+			"grug-far",
+			"simple_noice_input",
+			"qf",
+			"netrw",
+			"NvimTree",
+			"lazy",
+			"mason",
+			"TelescopePrompt",
+			"alpha",
+			"checkhealth",
+			"neo-tree",
+			"oil",
+		},
+	},
 }
