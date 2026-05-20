d5e0ccf080aa65f805062f3b1b412765910507af
thongvu <anhthongvu1996@gmail.com[D>
2026-05-20 23:19:43 +0700

    open preview file grug-far.nvim

--- dotfiles/nvim/.config/nvim/lua/config/plugins/code/grug-far.lua
+++ dotfiles/nvim/.config/nvim/lua/config/plugins/code/grug-far.lua
@@ -15,6 +15,35 @@ return {
 				vim.keymap.set("n", "q", "<Cmd>bd!<CR>", { buffer = true })
 			end,
 		})
+
+		vim.api.nvim_create_autocmd('FileType', {
+			group = vim.api.nvim_create_augroup('grug-far-auto-sync', { clear = true }),
+			pattern = { 'grug-far' },
+			callback = function(event)
+				local bufnr = event.buf
+
+				-- Listen to cursor movement events only within this specific grug-far buffer
+				vim.api.nvim_create_autocmd('CursorMoved', {
+				buffer = bufnr,
+				callback = function()
+					-- 1. Save the current grug-far window ID where the user is navigating
+					local grug_win = vim.api.nvim_get_current_win()
+					
+					local inst = require('grug-far').get_instance(bufnr)
+					
+					-- Safely execute location preview to avoid crashes in non-result areas
+					pcall(function()
+					-- 2. Open the file and line corresponding to the current search result
+					-- Note: This action temporarily shifts Neovim's focus to the code window
+					inst:open_location()
+					
+					-- 3. Forcefully restore focus back to the grug-far window
+					vim.api.nvim_set_current_win(grug_win)
+					end)
+				end,
+				})
+			end,
+		})
 	end,
 	keys = {
 		{
